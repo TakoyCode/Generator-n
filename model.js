ad514f0dd49b9ca10d20e220b65899ea44bb6af1
TakoyCode <129565940+TakoyCode@users.noreply.github.com>
2024-03-27 21:05:13 +0100

    Update model.js

--- model.js
+++ model.js
@@ -11,7 +11,7 @@ const model = {
     cards: [
         {
             sender: "Audun",
-            receiver: "Odin",
+            receiver: "Deg som leser dette",
             contents: `Dette er her tekst skal være, plis ikke skriv for mye den er sjenert`,
             picture: "https://i1.adis.ws/i/canon/future_of_forests_header_16x9_dc14bbe1e35040f79bf566eedaf5c8f7?$hero-header-half-16by9-dt$",
         },
@@ -22,7 +22,7 @@ const model = {
             picture: "https://www.columbiatribune.com/gcdn/authoring/2015/12/23/NCDT/ghows-MO-2188d3b8-4f3a-436a-b1d6-f3fa2f3aa210-a85f8cb1.jpeg?crop=1764,997,x0,y88&width=1764&height=997&format=pjpg&auto=webp",
         },
         {
-            sender: "Pappa",
+            sender: "Far",
             receiver: "Deg",
             contents: `
             Har du hørt om han som reklamerte på den nye kortstokken han hadde kjøpt?  
